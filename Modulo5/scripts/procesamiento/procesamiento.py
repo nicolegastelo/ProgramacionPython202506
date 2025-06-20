49c93705388b3dd1351c449873ce9263e8032e8b
gdelgador <gonzalo.delgado.r@uni.pe>
2025-06-19 23:27:38 -0500

    carga archivos

---
+++ Modulo5/scripts/procesamiento/procesamiento.py
@@ -0,0 +1,10 @@
+"""
+Crearemos un procesamiento de datos con pandas, a partir de una lectura de información.
+realizaremos algunos calculos solictados y luego guardaremos la información en cierto formato.
+
+Tienes un fichero ventas.csv que contiene datos de ventas en formato CSV.
+Cada línea del fichero tiene la siguiente estructura: fecha,producto,cantidad,precio_unitario.
+Debes leer el fichero, procesar los datos y calcular el total de ventas por producto.
+Finalmente, debes escribir los resultados en un nuevo fichero total_ventas.txt.
+"""
+
